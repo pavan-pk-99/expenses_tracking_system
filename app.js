2eb788d3c4f54fa073553d5a3b59074d2a1792b6
pavan-pk-99 <76556706+pavan-pk-99@users.noreply.github.com>
2024-07-20 09:02:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Router = require("./routers");
 dotenv.config({ path: "./config.env" });
 const app = express();
 
-const dbURI =" mongodb://localhost:27017/myapp";
+const dbURI ="mongodb+srv://pavankumarp2003:<password>@cluster0.j90wg4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const port = process.env.PORT || 5000;
 SECRET_KEY="mysecretkey"
 app.use(express.static("public"));
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(Router);
 mongoose
-  .connect("mongodb://localhost:27017/myapp")
+  .connect("mongodb+srv://pavankumarp2003:<password>@cluster0.j90wg4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then((result) => {
     app.listen(port);
     console.log("connected to mongodb and listening at port 5000");
